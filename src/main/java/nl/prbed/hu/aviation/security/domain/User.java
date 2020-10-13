5bb2c82a5cda2555831989582c37109d0469e52c
Arjen Norbart <arjennorbart@gmail.com>
2020-10-13 16:15:12 +0200

    Fix required changes for review

--- src/main/java/nl/prbed/hu/aviation/security/domain/User.java
+++ src/main/java/nl/prbed/hu/aviation/security/domain/User.java
@@ -3,12 +3,10 @@ package nl.prbed.hu.aviation.security.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
-import java.util.UUID;
-
 @Data
 @AllArgsConstructor
 public class User {
-    private UUID userId;
+    private Long userId;
     private String firstName;
     private String surName;
 }
