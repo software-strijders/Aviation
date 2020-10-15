e499dd90ffe6376601232c1d1914a8f907082029
Xander Vedder <xander.vedd@gmail.com>
2020-10-15 16:01:36 +0200

    Update domain layer

--- src/main/java/nl/prbed/hu/aviation/domain/factory/AircraftFactory.java
+++ src/main/java/nl/prbed/hu/aviation/domain/factory/AircraftFactory.java
@@ -3,20 +3,29 @@ package nl.prbed.hu.aviation.domain.factory;
 import lombok.RequiredArgsConstructor;
 import nl.prbed.hu.aviation.data.aircraft.AircraftEntity;
 import nl.prbed.hu.aviation.domain.Aircraft;
-import nl.prbed.hu.aviation.domain.Type;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Component
 @RequiredArgsConstructor
 public class AircraftFactory {
-    private TypeFactory typeFactory;
+    private final TypeFactory typeFactory;
 
-    public Aircraft createFromEntity(AircraftEntity entity) {
+    public Aircraft from(AircraftEntity entity) {
         return new Aircraft(
                 entity.getCode(),
-                typeFactory.createFromEntity(entity.getType()),
+                this.typeFactory.from(entity.getType()),
                 null,
                 null,
                 null);
+    }
+
+    public List<Aircraft> from(List<AircraftEntity> entities) {
+        var list = new ArrayList<Aircraft>();
+        for (var entity : entities)
+            list.add(this.from(entity));
+        return list;
     }
 }
