e0a92160ef68453d68ca8ecb082ff6a1ae67e52c
Xander Vedder <xander.vedd@gmail.com>
2020-10-15 16:02:19 +0200

    Update presentation layer
    Added several new Dto's and also added the required Get endpoints.
    
    closes #145, closes #146, closes #147, closes #148

--- src/main/java/nl/prbed/hu/aviation/presentation/aircraft/controller/AircraftController.java
+++ src/main/java/nl/prbed/hu/aviation/presentation/aircraft/controller/AircraftController.java
@@ -3,9 +3,7 @@ package nl.prbed.hu.aviation.presentation.aircraft.controller;
 import lombok.AllArgsConstructor;
 import nl.prbed.hu.aviation.application.AircraftService;
 import nl.prbed.hu.aviation.application.TypeService;
-import nl.prbed.hu.aviation.presentation.aircraft.dto.CreateAircraftDto;
-import nl.prbed.hu.aviation.presentation.aircraft.dto.CreateTypeDto;
-import nl.prbed.hu.aviation.presentation.aircraft.dto.DeleteAircraftDto;
+import nl.prbed.hu.aviation.presentation.aircraft.dto.*;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.annotation.Validated;
@@ -19,35 +17,45 @@ public class AircraftController {
     private final AircraftService aircraftService;
     private final TypeService typeService;
 
-    @PostMapping
-    public void create(@Validated @RequestBody CreateAircraftDto dto) {
-        aircraftService.create(
-                dto.code,
-                dto.modelName
-        );
+    @GetMapping
+    public AircraftOverviewResponseDto findAll() {
+        return new AircraftOverviewResponseDto(this.aircraftService.findAll());
+    }
+
+    @GetMapping("/{modelName}")
+    public AircraftOverviewByTypeResponseDto findAllByModel(@PathVariable String modelName) {
+        return new AircraftOverviewByTypeResponseDto(modelName, this.aircraftService.findAllByType(modelName));
     }
 
     @DeleteMapping("/{code}")
     public void delete(@Validated @PathVariable String code) {
-        aircraftService.delete(code);
+        this.aircraftService.delete(code);
+    }
+
+    @Transactional
+    @DeleteMapping("/type/{model}")
+    public void deleteType(@Validated @PathVariable String model) {
+        this.aircraftService.deleteByType(model);
+        this.typeService.delete(model);
+    }
+
+    @PostMapping("/create")
+    public CreateAircraftResponseDto create(@Validated @RequestBody CreateAircraftDto dto) {
+        var aircraft = this.aircraftService.create(dto.code, dto.modelName);
+        return new CreateAircraftResponseDto(aircraft.getCode(), aircraft.getType());
     }
 
     @PostMapping("/type")
-    public void createType(@Validated @RequestBody CreateTypeDto dto) {
-        typeService.create(
+    public CreateTypeResponseDto create(@Validated @RequestBody CreateTypeDto dto) {
+        var type = this.typeService.create(
                 dto.modelName,
                 dto.manufacturer,
                 dto.fuelCapacity,
                 dto.fuelConsumption,
                 dto.numSeatsFirst,
                 dto.numSeatsBusiness,
-                dto.numSeatsEconomy);
-    }
-
-    @Transactional
-    @DeleteMapping("/type/{model}")
-    public void deleteType(@Validated @PathVariable String model) {
-        aircraftService.deleteByType(model);
-        typeService.delete(model);
+                dto.numSeatsEconomy
+        );
+        return new CreateTypeResponseDto(type);
     }
 }
