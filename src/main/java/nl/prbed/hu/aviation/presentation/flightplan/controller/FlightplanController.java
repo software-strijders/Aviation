dd284fa011f32cfd361fb94d83956003858f149c
Rubenvdbrink <rubenvandenbrink@hotmail.com>
2020-10-15 13:45:55 +0200

    Add get to FlightplanController

--- src/main/java/nl/prbed/hu/aviation/presentation/flightplan/controller/FlightplanController.java
+++ src/main/java/nl/prbed/hu/aviation/presentation/flightplan/controller/FlightplanController.java
@@ -3,12 +3,10 @@ package nl.prbed.hu.aviation.presentation.flightplan.controller;
 import lombok.AllArgsConstructor;
 import nl.prbed.hu.aviation.application.FlightplanService;
 import nl.prbed.hu.aviation.presentation.flightplan.dto.CreateFlightplanDto;
+import nl.prbed.hu.aviation.presentation.flightplan.dto.GetFlightplanDto;
 import nl.prbed.hu.aviation.presentation.flightplan.dto.ResponseFlightplanDto;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/flightplan")
@@ -16,9 +14,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class FlightplanController {
     private final FlightplanService flightplanService;
 
+    //TODO: fix destination and arrival
     @PostMapping("/create")
     public ResponseFlightplanDto create(@RequestBody CreateFlightplanDto dto) {
         var flightplan = flightplanService.create(dto.duration, dto.code);
+        return new ResponseFlightplanDto(flightplan.getCode(), flightplan.getDuration());
+    }
+
+    //TODO: fix destination and arrival
+    @GetMapping("/get")
+    public ResponseFlightplanDto get(@RequestBody GetFlightplanDto dto) {
+        var flightplan = flightplanService.get(dto.code);
         return new ResponseFlightplanDto(flightplan.getCode(), flightplan.getDuration());
     }
 }
