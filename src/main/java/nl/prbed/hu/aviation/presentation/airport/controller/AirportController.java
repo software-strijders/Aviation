7c683ce6e2e5a0c5291e801b28636ccb2d9a9119
Arjen Norbart <arjennorbart@gmail.com>
2020-10-15 13:04:36 +0200

    Implement presentation layer

---
+++ src/main/java/nl/prbed/hu/aviation/presentation/airport/controller/AirportController.java
@@ -0,0 +1,39 @@
+package nl.prbed.hu.aviation.presentation.airport.controller;
+
+import lombok.AllArgsConstructor;
+import nl.prbed.hu.aviation.application.AirportService;
+import nl.prbed.hu.aviation.application.CityService;
+import nl.prbed.hu.aviation.presentation.dto.CreateAirportDto;
+import nl.prbed.hu.aviation.presentation.dto.CreateCityDto;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/airport")
+@AllArgsConstructor
+public class AirportController {
+    private final AirportService airportService;
+    private final CityService cityService;
+
+    @PostMapping
+    public void create(@Validated @RequestBody CreateAirportDto dto) {
+        airportService.create(
+                dto.code,
+                dto.latitude,
+                dto.longitude,
+                dto.cityName
+                );
+    }
+
+    @PostMapping("/city")
+    public void create(@Validated @RequestBody CreateCityDto dto) {
+        airportService.createCity(
+                dto.name,
+                dto.country,
+                dto.airportCode
+        );
+    }
+}
---
+++ src/main/java/nl/prbed/hu/aviation/presentation/dto/CreateCityDto.java
@@ -0,0 +1,7 @@
+package nl.prbed.hu.aviation.presentation.dto;
+
+public class CreateCityDto {
+    public String name;
+    public String country;
+    public String airportCode;
+}
