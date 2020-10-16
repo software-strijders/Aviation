22227f95ba1a0faa53385df4504d293c43985094
Arjen Norbart <arjennorbart@gmail.com>
2020-10-16 15:07:02 +0200

    fixed typo

--- src/main/java/nl/prbed/hu/aviation/application/exception/AirportAlreadyExistsException.java
+++ src/main/java/nl/prbed/hu/aviation/application/exception/AirportAlreadyExistsException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class AirportAlreadyExistsException extends RuntimeException {
     public AirportAlreadyExistsException(String code) {
-        super(String.format("Airport with code: '%s' already excists", code));
+        super(String.format("Airport with code: '%s' already exists", code));
     }
 }
--- src/main/java/nl/prbed/hu/aviation/application/exception/CityAlreadyExistsException.java
+++ src/main/java/nl/prbed/hu/aviation/application/exception/CityAlreadyExistsException.java
@@ -6,6 +6,6 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @ResponseStatus(code = HttpStatus.BAD_REQUEST)
 public class CityAlreadyExistsException extends RuntimeException {
     public CityAlreadyExistsException(String name) {
-        super(String.format("City with code: '%s' already excists", name));
+        super(String.format("City with code: '%s' already exists", name));
     }
 }
