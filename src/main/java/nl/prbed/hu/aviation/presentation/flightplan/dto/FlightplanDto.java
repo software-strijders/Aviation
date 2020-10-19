d0538d30e8a163247e7facae6e66abaa341d804a
JortWillemsen <jort@maurice-nienke.nl>
2020-10-19 17:22:33 +0200

    Add Swagger annotations to flightplan dtoss

--- src/main/java/nl/prbed/hu/aviation/presentation/flightplan/dto/FlightplanDto.java
+++ src/main/java/nl/prbed/hu/aviation/presentation/flightplan/dto/FlightplanDto.java
@@ -1,7 +1,13 @@
 package nl.prbed.hu.aviation.presentation.flightplan.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("Details about the flight plan")
 public class FlightplanDto {
+    @ApiModelProperty(notes = "The code of the flight plan")
     public String code;
+    @ApiModelProperty(notes = "The duration of the flight plan")
     public Long duration;
     public String arrival;
     public String destination;
--- src/main/java/nl/prbed/hu/aviation/presentation/flightplan/dto/FlightplanFindByCodeResponseDto.java
+++ src/main/java/nl/prbed/hu/aviation/presentation/flightplan/dto/FlightplanFindByCodeResponseDto.java
@@ -1,14 +1,21 @@
 package nl.prbed.hu.aviation.presentation.flightplan.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import nl.prbed.hu.aviation.domain.Airport;
 
 @Getter
 @RequiredArgsConstructor
+@ApiModel(description = "Details about the flight plan")
 public class FlightplanFindByCodeResponseDto {
+    @ApiModelProperty(notes = "The code of the flight plan")
     private final String code;
+    @ApiModelProperty(notes = "The duration of the flight plan")
     private final Long duration;
+    @ApiModelProperty(notes = "The arrival airport of the flight plan")
     private final Airport arrival;
+    @ApiModelProperty(notes = "The destination airport of the flight plan")
     private final Airport destination;
 }
