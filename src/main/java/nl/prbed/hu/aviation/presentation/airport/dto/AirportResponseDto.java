0623e57deb1fe55a84fc28ae20b4201903abf364
JortWillemsen <jort@maurice-nienke.nl>
2020-10-19 17:19:19 +0200

    Add Swagger annotations to airport dtos

--- src/main/java/nl/prbed/hu/aviation/presentation/airport/dto/AirportResponseDto.java
+++ src/main/java/nl/prbed/hu/aviation/presentation/airport/dto/AirportResponseDto.java
@@ -1,13 +1,20 @@
 package nl.prbed.hu.aviation.presentation.airport.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 @Getter
 @RequiredArgsConstructor
+@ApiModel(description = "Details about the airport")
 public class AirportResponseDto {
+    @ApiModelProperty(notes = "The code of the airport")
     private final String code;
+    @ApiModelProperty(notes = "The latitude of the airport in signed degrees format")
     private final double latitude;
+    @ApiModelProperty(notes = "The longitude of the airport in signed degrees format")
     private final double longitude;
+    @ApiModelProperty(notes = "The longitude of the airport in signed degrees format")
     private final String cityName;
 }
