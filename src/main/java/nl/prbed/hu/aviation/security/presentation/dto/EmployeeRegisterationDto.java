b00ac752d05bfd25aedaba7fff37e1565e0d5667
JortWillemsen <jort@maurice-nienke.nl>
2020-10-20 12:03:56 +0200

    Add Swagger annotations to registration and login dtos

--- src/main/java/nl/prbed/hu/aviation/security/presentation/dto/EmployeeRegisterationDto.java
+++ src/main/java/nl/prbed/hu/aviation/security/presentation/dto/EmployeeRegisterationDto.java
@@ -1,18 +1,24 @@
 package nl.prbed.hu.aviation.security.presentation.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 
 public class EmployeeRegisterationDto {
     @NotBlank
+    @ApiModelProperty(notes = "The unique username of the employee")
     public String username;
 
+    @ApiModelProperty(notes = "The password. This must be at least 5 characters long")
     @Size(min = 5)
     public String password;
 
+    @ApiModelProperty(notes = "The first name or names.")
     @NotBlank
     public String firstName;
 
+    @ApiModelProperty(notes = "The last name. With prefix")
     @NotBlank
     public String lastName;
 }
--- src/main/java/nl/prbed/hu/aviation/security/presentation/dto/LoginDto.java
+++ src/main/java/nl/prbed/hu/aviation/security/presentation/dto/LoginDto.java
@@ -1,6 +1,11 @@
 package nl.prbed.hu.aviation.security.presentation.dto;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class LoginDto {
+    @ApiModelProperty(notes = "The unique username of the customer")
     public String username;
+
+    @ApiModelProperty(notes = "The password. This must be at least 5 characters long")
     public String password;
 }
