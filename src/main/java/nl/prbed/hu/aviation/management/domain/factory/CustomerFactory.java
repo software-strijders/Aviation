f0e4970e58529d748df33c786b2d902a86fcd206
Xander Vedder <xander.vedd@gmail.com>
2020-10-22 16:26:53 +0200

    Update CustomerFactory.java

--- src/main/java/nl/prbed/hu/aviation/management/domain/factory/CustomerFactory.java
+++ src/main/java/nl/prbed/hu/aviation/management/domain/factory/CustomerFactory.java
@@ -4,6 +4,9 @@ import nl.prbed.hu.aviation.management.data.user.CustomerEntity;
 import nl.prbed.hu.aviation.management.domain.Customer;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Component
 public class CustomerFactory {
     public Customer from(CustomerEntity entity) {
@@ -18,5 +21,9 @@ public class CustomerFactory {
                 // TODO: Add bookings to Customer
                 null
         );
+    }
+
+    public List<Customer> from(List<CustomerEntity> entities) {
+        return entities.stream().map(this::from).collect(Collectors.toList());
     }
 }
