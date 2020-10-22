90a3a74c69134dc2ce09c3320fc80ae78167bef6
Xander Vedder <xander.vedd@gmail.com>
2020-10-22 16:30:16 +0200

    Add CustomerService.java
    Note, that there will be no creation in this Service. That is the
    responsibility of the RegistrationController which handles hashing
    the passwords et cetera.

---
+++ src/main/java/nl/prbed/hu/aviation/management/application/CustomerService.java
@@ -0,0 +1,39 @@
+package nl.prbed.hu.aviation.management.application;
+
+import lombok.RequiredArgsConstructor;
+import nl.prbed.hu.aviation.management.application.exception.EntityNotFoundException;
+import nl.prbed.hu.aviation.management.data.user.CustomerEntity;
+import nl.prbed.hu.aviation.management.domain.Customer;
+import nl.prbed.hu.aviation.management.domain.factory.CustomerFactory;
+import nl.prbed.hu.aviation.security.data.SpringUserRepository;
+import nl.prbed.hu.aviation.security.data.User;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@RequiredArgsConstructor
+public class CustomerService {
+    private static final String ERROR_MSG = "Could not find customer with id '%s'";
+
+    private final SpringUserRepository userRepository;
+    private final CustomerFactory factory;
+
+    public List<Customer> findAllCustomers() {
+        var entities = this.userRepository.findAllCustomers().stream()
+                .map(this::map).collect(Collectors.toList());
+        return this.factory.from(entities);
+    }
+
+    public Customer findById(Long id) {
+        var entity = this.userRepository.findByIdAndCustomer(id)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(ERROR_MSG, id)));
+
+        return this.factory.from(this.map(entity));
+    }
+
+    private CustomerEntity map(User user) {
+        return (CustomerEntity) user;
+    }
+}
