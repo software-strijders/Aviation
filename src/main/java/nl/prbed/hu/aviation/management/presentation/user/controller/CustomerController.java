d4e431fc34ae779120047cf95fa9f9c6677cf16e
Arjen Norbart <arjennorbart@gmail.com>
2020-10-23 10:50:07 +0200

    Implement layers

--- src/main/java/nl/prbed/hu/aviation/management/presentation/user/controller/CustomerController.java
+++ src/main/java/nl/prbed/hu/aviation/management/presentation/user/controller/CustomerController.java
@@ -6,10 +6,8 @@ import nl.prbed.hu.aviation.management.application.CustomerService;
 import nl.prbed.hu.aviation.management.presentation.user.dto.CustomerOverviewResponseDto;
 import nl.prbed.hu.aviation.management.presentation.user.dto.CustomerResponseDto;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -40,5 +38,11 @@ public class CustomerController {
                 customer.getEmail(),
                 customer.getPhoneNumber()
         );
+    }
+
+    @ApiOperation(value = "Delete a customer")
+    @DeleteMapping("/{username}")
+    public void deleteCustomer(@PathVariable String username) {
+        this.service.deleteCustomer(username);
     }
 }
