ccc9f5e288283199ca3def1047d28a560b3606de
Rubenvdbrink <rubenvandenbrink@hotmail.com>
2020-10-23 16:58:13 +0200

    Add Employee to domain

--- src/main/java/nl/prbed/hu/aviation/management/domain/Employee.java
+++ src/main/java/nl/prbed/hu/aviation/management/domain/Employee.java
@@ -1,4 +1,13 @@
 package nl.prbed.hu.aviation.management.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+@AllArgsConstructor
 public class Employee {
+    private String username;
+    private String firstName;
+    private String lastName;
 }
