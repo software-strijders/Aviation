56d584a54cf0c91b73a7838d590070d743077015
Rubenvdbrink <rubenvandenbrink@hotmail.com>
2020-10-23 16:59:40 +0200

    Add findAll and findByUsername (employee) to controller

--- src/main/java/nl/prbed/hu/aviation/management/presentation/user/controller/EmployeeController.java
+++ src/main/java/nl/prbed/hu/aviation/management/presentation/user/controller/EmployeeController.java
@@ -3,22 +3,41 @@ package nl.prbed.hu.aviation.management.presentation.user.controller;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import nl.prbed.hu.aviation.management.application.EmployeeService;
+import nl.prbed.hu.aviation.management.presentation.user.dto.EmployeeOverviewResponseDto;
+import nl.prbed.hu.aviation.management.presentation.user.dto.EmployeeResponseDto;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @Secured("ROLE_EMPLOYEE")
 @RequestMapping("/employee")
 public class EmployeeController {
-    private final EmployeeService service;
+    private final EmployeeService employeeService;
 
-    @ApiOperation(value = "Delete an employee")
+    @ApiOperation(
+            value = "Delete an employee",
+            notes = "Provide the username of the user."
+    )
     @DeleteMapping("/{username}")
     public void deleteCustomer(@PathVariable String username) {
-        this.service.deleteEmployee(username);
+        this.employeeService.deleteEmployee(username);
+    }
+
+    @ApiOperation(
+            value = "Find an employee",
+            notes = "Provide the username of the user."
+    )
+    @GetMapping("/{username}")
+    public EmployeeResponseDto findByUsername(@PathVariable String username) {
+        var employee = this.employeeService.findByUsername(username);
+        return new EmployeeResponseDto(employee.getUsername(), employee.getFirstName(), employee.getLastName());
+    }
+
+    @ApiOperation(value = "Find all employees")
+    @GetMapping
+    public EmployeeOverviewResponseDto findAll() {
+        var employees = this.employeeService.findAll();
+        return new EmployeeOverviewResponseDto(employees);
     }
 }
