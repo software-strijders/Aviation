a6f10f40f25e81e95bfef067d267275da370ab8b
Rubenvdbrink <rubenvandenbrink@hotmail.com>
2020-10-24 14:11:32 +0200

    Add delete flight function to service

--- src/main/java/nl/prbed/hu/aviation/management/application/FlightService.java
+++ src/main/java/nl/prbed/hu/aviation/management/application/FlightService.java
@@ -1,16 +1,19 @@
 package nl.prbed.hu.aviation.management.application;
 
 import lombok.RequiredArgsConstructor;
+import nl.prbed.hu.aviation.management.application.exception.EntityNotFoundException;
 import nl.prbed.hu.aviation.management.data.flight.FlightEntity;
 import nl.prbed.hu.aviation.management.data.flight.SpringFlightRepository;
 import nl.prbed.hu.aviation.management.domain.Flight;
-import nl.prbed.hu.aviation.management.domain.Flightplan;
 import nl.prbed.hu.aviation.management.domain.factory.FlightFactory;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @RequiredArgsConstructor
 public class FlightService {
+    private static final String ERROR_MSG = "Could not find flight with code '%s'";
+
     private final SpringFlightRepository flightRepository;
     private final AircraftService aircraftService;
     private final FlightplanService flightplanService;
@@ -28,5 +31,10 @@ public class FlightService {
         var flightplan = this.flightplanService.findFlightplanEntityByCode(flightPlanCode);
         var entity = new FlightEntity(code, priceEconomy, priceBusiness, priceFirst, aircraft, flightplan);
         return flightFactory.from(flightRepository.save(entity));
+    }
+
+    public void deleteByCode(String code) {
+        this.flightRepository.delete(this.flightRepository.findFlightEntityByCode(code)
+                .orElseThrow(() -> new EntityNotFoundException(String.format(ERROR_MSG, code))));
     }
 }
