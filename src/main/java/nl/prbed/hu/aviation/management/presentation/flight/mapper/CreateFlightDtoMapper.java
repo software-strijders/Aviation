9405161afb5780bc22e8243f2f97148d91d01307
Rubenvdbrink <rubenvandenbrink@hotmail.com>
2020-10-26 14:52:18 +0100

    Create CreateFlightDtoMapper

--- src/main/java/nl/prbed/hu/aviation/management/presentation/flight/mapper/CreateFlightDtoMapper.java
+++ src/main/java/nl/prbed/hu/aviation/management/presentation/flight/mapper/CreateFlightDtoMapper.java
@@ -1,4 +1,13 @@
 package nl.prbed.hu.aviation.management.presentation.flight.mapper;
 
+import nl.prbed.hu.aviation.management.application.struct.FlightStruct;
+import nl.prbed.hu.aviation.management.presentation.flight.dto.FlightDto;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface CreateFlightDtoMapper {
+    CreateFlightDtoMapper instance = Mappers.getMapper(CreateFlightDtoMapper.class);
+
+    FlightStruct toFlightStruct(FlightDto dto);
 }
