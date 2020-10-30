97e9f88aedbb73c9ce96519979c63750c4eb8fca
Xander Vedder <xander.vedd@gmail.com>
2020-10-30 21:18:30 +0100

    Add HATEOAS to EmployeeController.java

--- src/main/java/nl/prbed/hu/aviation/management/presentation/user/controller/EmployeeController.java
+++ src/main/java/nl/prbed/hu/aviation/management/presentation/user/controller/EmployeeController.java
@@ -4,19 +4,26 @@ import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import nl.prbed.hu.aviation.management.application.EmployeeService;
 import nl.prbed.hu.aviation.management.domain.Employee;
+import nl.prbed.hu.aviation.management.presentation.hateoas.HateoasBuilder;
+import nl.prbed.hu.aviation.management.presentation.hateoas.HateoasDirector;
+import nl.prbed.hu.aviation.management.presentation.hateoas.HateoasType;
 import nl.prbed.hu.aviation.management.presentation.user.dto.EmployeeOverviewResponseDto;
 import nl.prbed.hu.aviation.management.presentation.user.dto.EmployeeResponseDto;
 import nl.prbed.hu.aviation.management.presentation.user.dto.EmployeeUpdateDto;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.security.access.annotation.Secured;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
 @Secured("ROLE_EMPLOYEE")
 @RequestMapping("/employee")
 public class EmployeeController {
+    private final HateoasDirector hateoasDirector = new HateoasDirector(new HateoasBuilder(), this.getClass());
     private final EmployeeService service;
 
     @ApiOperation(
@@ -33,16 +40,21 @@ public class EmployeeController {
             notes = "Provide the username of the employee."
     )
     @GetMapping("/{username}")
-    public EmployeeResponseDto findByUsername(@PathVariable String username) {
+    public EntityModel<EmployeeResponseDto> findByUsername(@PathVariable String username) {
         var employee = this.service.findByUsername(username);
-        return new EmployeeResponseDto(employee.getUsername(), employee.getFirstName(), employee.getLastName());
+        var response = this.createResponseDto(employee);
+        return EntityModel.of(response, this.hateoasDirector.make(HateoasType.FIND_ONE, username));
     }
 
     @ApiOperation(value = "Find all employees")
     @GetMapping
-    public EmployeeOverviewResponseDto findAll() {
+    public CollectionModel<EntityModel<EmployeeResponseDto>> findAll() {
         var employees = this.service.findAll();
-        return new EmployeeOverviewResponseDto(employees);
+        var response = employees.stream()
+                .map(this::createResponseDto)
+                .map(dto -> EntityModel.of(dto, this.hateoasDirector.make(HateoasType.FIND_ONE, dto.getUsername())))
+                .collect(Collectors.toList());
+        return CollectionModel.of(response, this.hateoasDirector.make(HateoasType.FIND_ALL));
     }
 
     @ApiOperation(
@@ -50,8 +62,13 @@ public class EmployeeController {
             notes = "Provide the username of the employee"
     )
     @PatchMapping("/{username}")
-    public EmployeeResponseDto update(@PathVariable String username, @Valid @RequestBody EmployeeUpdateDto dto) {
-        return this.createResponseDto(this.service.update(username, dto.firstName, dto.lastName));
+    public EntityModel<EmployeeResponseDto> update(
+            @PathVariable String username,
+            @Valid @RequestBody EmployeeUpdateDto dto
+    ) {
+        var employee = this.service.update(username, dto.firstName, dto.lastName);
+        var response = this.createResponseDto(employee);
+        return EntityModel.of(response, this.hateoasDirector.make(HateoasType.UPDATE, username));
     }
 
     private EmployeeResponseDto createResponseDto(Employee employee) {
