a2d5b7536d2ccf9c5b59a2ba7185a2571d0c2f23
Arjen Norbart <arjennorbart@gmail.com>
2020-11-02 17:13:05 +0100

    Implement domain layer

--- src/main/java/nl/prbed/hu/aviation/management/domain/SeatType.java
+++ src/main/java/nl/prbed/hu/aviation/management/domain/SeatType.java
@@ -1,7 +1,22 @@
 package nl.prbed.hu.aviation.management.domain;
 
+import nl.prbed.hu.aviation.management.application.exception.SearchFlightDetailsException;
+
+import java.util.stream.Stream;
+
 public enum SeatType {
-    ECONOMY,
-    BUSINESS,
-    FIRST
+    ECONOMY("ECONOMY"),
+    BUSINESS("BUSINESS"),
+    FIRST("FIRST");
+
+    private final String seat;
+
+    SeatType(String seat) {
+        this.seat = seat;
+    }
+
+    public static SeatType fromString(String seat) {
+        return Stream.of(SeatType.values()).filter(seatType -> seatType.seat.equals(seat))
+                .findFirst().orElseThrow(() -> new SearchFlightDetailsException("Invalid class"));
+    }
 }
