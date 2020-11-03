f4d01d3ba5c632d3382016654d11e1e43850a5a3
JortWillemsen <jort@maurice-nienke.nl>
2020-11-03 15:55:08 +0100

    Add Grafana to Spring

--- src/main/java/nl/prbed/hu/aviation/AviationApplication.java
+++ src/main/java/nl/prbed/hu/aviation/AviationApplication.java
@@ -1,7 +1,11 @@
 package nl.prbed.hu.aviation;
 
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableSwagger2
@@ -9,5 +13,11 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class AviationApplication {
     public static void main(String[] args) {
         SpringApplication.run(AviationApplication.class, args);
+    }
+
+    @Bean
+    MeterRegistryCustomizer<MeterRegistry> configurer(
+            @Value("${spring.application.name}") String applicationName) {
+        return registry -> registry.config().commonTags("application", applicationName);
     }
 }
