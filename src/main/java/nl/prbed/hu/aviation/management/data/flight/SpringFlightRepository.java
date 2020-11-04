001ffe4db9bbd622d2d79d7627a94324e568fe29
JortWillemsen <jort@maurice-nienke.nl>
2020-11-04 13:44:30 +0100

    Add method to get all flights by an aircraft

--- src/main/java/nl/prbed/hu/aviation/management/data/flight/SpringFlightRepository.java
+++ src/main/java/nl/prbed/hu/aviation/management/data/flight/SpringFlightRepository.java
@@ -1,5 +1,6 @@
 package nl.prbed.hu.aviation.management.data.flight;
 
+import nl.prbed.hu.aviation.management.data.aircraft.AircraftEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -9,4 +10,5 @@ public interface SpringFlightRepository extends JpaRepository<FlightEntity, Long
     Optional<FlightEntity> findFlightEntityByCode(String code);
     List<FlightEntity> findFlightEntitiesByFlightplanDepartureCode(String code);
     List<FlightEntity> findFlightEntitiesByFlightplanDestinationCode(String code);
+    List<FlightEntity> findFlightEntitiesByAircraft(AircraftEntity entity);
 }
